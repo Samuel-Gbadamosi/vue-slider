c4a281ebc22f9166bcb3fdac3481ddecd17f5612
samuel <damosisamuel@gmail.com>
2021-01-26 15:52:21 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    counter : 0,
+    immagine : [
+      'img/img1.webp',
+      'img/img2.webp',
+      'img/img3.jpg',
+      'img/img4.webp'
+  ]
+},
+methods: {
+  nextPhoto (){
+
+    this.counter++;
+    console.log(this.counter);
+    if (this.counter == (this.immagine.length)){
+      this.counter = 0;
+    }
+  },
+  prevPhoto(){
+  this.counter--;
+    if (this.counter < 0 ){
+      this.counter = this.immagine.length -1
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
